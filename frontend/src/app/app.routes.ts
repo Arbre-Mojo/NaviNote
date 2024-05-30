a8341804f8aebbb4a6814037fb4707222cdc8781
Rifle1 <philipe.barakat@yahoo.com>
2024-05-30 14:45:09 +0200

    front end changes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TimeTableComponent} from "./component/time-table/time-table.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'time-table', component: TimeTableComponent},
+];
